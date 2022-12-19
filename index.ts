8ab25dea6a6cb3bed8a4ee3f35b0dd4b92079ca9
aoshea <aoshea@users.noreply.github.com>
2022-12-19 10:05:41 +0000

    heptagon

--- index.ts
+++ index.ts
@@ -1,6 +1,42 @@
 // Import stylesheets
 import './style.css';
 
+const SVG_NS = 'http://www.w3.org/2000/svg';
+
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>ZZ</h1>`;
+
+const artboard = document.querySelector('svg');
+
+interface Vec2D {
+  x: number;
+  y: number;
+}
+
+function getPolygonPoints(n: number, radius: number, centre: Vec2D): string {
+  const angle_in_radians = Math.PI / n;
+  let current_angle = 0;
+
+  const points = [];
+  for (let i = 0; i < n; ++i) {
+    points.push({
+      x: Math.floor(centre.x + Math.cos(current_angle) * radius),
+      y: Math.floor(centre.y + Math.sin(current_angle) * radius),
+    });
+    current_angle += angle_in_radians;
+  }
+
+  return points.map((p) => `${p.x},${p.y}`).join(' ');
+}
+
+function createSVGPolygon(): SVGPolygonElement {
+  const polygon = document.createElementNS(SVG_NS, 'polygon');
+  polygon.setAttribute('points', getPolygonPoints(7, 10, { x: 50, y: 50 }));
+  polygon.setAttribute('fill', 'red');
+  polygon.setAttribute('stroke', 'blue');
+  return polygon;
+}
+
+const polygon = createSVGPolygon();
+artboard.appendChild(polygon);
